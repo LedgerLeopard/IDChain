e8d711ac15bc901585ef00751d4065e1b29ce0d0
sergeybrazhnik <6epreu@gmail.com>
2018-06-07 21:05:47 +0300

    added login page with mvp

--- app/src/main/java/com/ledgerleopard/sorvin/SplashActivity.java
+++ app/src/main/java/com/ledgerleopard/sorvin/SplashActivity.java
@@ -5,12 +5,15 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import com.ledgerleopard.sorvin.functionality.login.LoginViewImpl;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Fabric.with(this, new Crashlytics());
 
 		new Handler().postDelayed(() -> {
 			LoginViewImpl.start(this);
